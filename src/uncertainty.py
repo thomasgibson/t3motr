a2892b24e29a624fad9b4cfff87006ec945efff0
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-06-27 15:03:46 +0100

    Sket out uncertainty class

---
+++ src/uncertainty.py
@@ -0,0 +1,26 @@
+"""
+Uncertainty classes.
+"""
+
+class UncertaintyBase(object):
+    """
+    """
+
+    def __init__(self, reference, forecast):
+        self._reference = reference
+        self._forecast = forecast
+        self._setup()
+
+    def _setup(self):
+        raise NotImplementedError("Method not implemented.")
+
+    
+class NormalizedErrorAirPressure(UncertaintyBase):
+
+    def _setup(self):
+        ref_pr = self._reference['air_pressure_at_sea_level'][2]
+        forecast_pr = self._forecast['air_pressure_at_sea_level'][2]
+        sum_ref = sum(ref_pr)
+        err = sum([ref - forecast for ref, forecast in zip(ref_pr, forecast_pr)])
+        norm_err = err / sum_ref
+        self.norm_err = norm_err
