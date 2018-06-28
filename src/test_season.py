008aaae8e954ed757c996e052316a6331afa4284
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-06-28 15:49:02 +0100

    Update season script

--- src/test_season.py
+++ src/test_season.py
@@ -6,11 +6,10 @@ from netCDF4 import Dataset
 from pathlib import Path
 import os
 
-days = np.arange(2, 10, 1)
+days = np.arange(2, 31, 1)
 times = [9]
 years = [2013, 2014, 2015, 2016]
-months = [1]
-uncertainties = {}
+months = range(1, 13)
 output_name = 'uncertainty_output.nc'
 
 if os.path.isfile('./'+str(output_name)) is not True:
@@ -30,69 +29,54 @@ p_array = output_data.variables['pressure_error']
 T_array = output_data.variables['temperature_error']
 r_array = output_data.variables['rainfall_error']
 
+# Lead time of 3hrs
+lead_time = 3
 
-# for now let's do lead time of 27 hours for now
-lead_time = 3  # so for a time of 9 and lead time of 9, we are looking at 12pm
-month_map = {1: "January",
-             2: "Feburary",
-             3: "March",
-             4: "April",
-             5: "May",
-             6: "June",
-             7: "July",
-             8: "August",
-             9: "September",
-             10: "October",
-             11: "November",
-             12: "December"}
-season_data = {}
-year = years[0]
+for year in years:
+    for month in months:
+        for day in days:
+            for time in times:
+                forecast_name = download_data('mogreps-uk',
+                                              make_data('mogreps-uk',
+                                                        year,
+                                                        month,
+                                                        day - 1,
+                                                        time,
+                                                        0,
+                                                        lead_time),
+                                              data_folder=Path('.'))
 
-for month in months:
-    for day in days:
-        for time in times:
+                reference_name = download_data('mogreps-uk',
+                                               make_data('mogreps-uk',
+                                                         year,
+                                                         month,
+                                                         day,
+                                                         time,
+                                                         0,
+                                                         lead_time),
+                                               data_folder=Path('.'))
 
-            forecast_name = download_data('mogreps-uk',
-                                          make_data('mogreps-uk',
-                                                    year,
-                                                    month,
-                                                    day - 1,
-                                                    time,
-                                                    0,
-                                                    lead_time),
-                                          data_folder=Path('.'))
+                try:
+                    forecast_data = Dataset(forecast_name, 'r')
+                    reference_data = Dataset(reference_name, 'r')
+                    measurerer = un.NormalizedErrors(reference_data, forecast_data)
+                    idx = output_data.dimensions['id'].size
 
-            reference_name = download_data('mogreps-uk',
-                                           make_data('mogreps-uk',
-                                                     year,
-                                                     month,
-                                                     day,
-                                                     time,
-                                                     0,
-                                                     lead_time),
-                                           data_folder=Path('.'))
+                    date_array[idx] = str(year)+'_'+str(month)+'_'+str(day)
 
-            try:
-                forecast_data = Dataset(forecast_name, 'a')
-                reference_data = Dataset(reference_name, 'a')
-                measurerer = un.NormalizedErrors(reference_data, forecast_data)
-                idx = output_data.dimensions['id'].size
+                    p_array[idx] = measurerer.p_err
+                    T_array[idx] = measurerer.T_err
+                    r_array[idx] = measurerer.r_err
+                except FileNotFoundError:
+                    pass
 
-                date_array[idx] = str(year)+'_'+str(month)+'_'+str(day)
+            os.remove(str(forecast_name))
 
-                p_array[idx] = measurerer.p_err
-                T_array[idx] = measurerer.T_err
-                r_array[idx] = measurerer.r_err
-            except FileNotFoundError:
-                pass
-
-
-        os.remove(str(forecast_name))
-try:
-    os.remove("prods*")
-except FileNotFoundError:
-    print("No files found to delete.")
-    pass
+    try:
+        os.remove("prods*")
+    except FileNotFoundError:
+        print("No files found to delete.")
+        pass
 
 print("date", date_array[:])
 print("pressure", p_array[:])
