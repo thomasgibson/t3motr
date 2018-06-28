5b93bd465bbe098fe0189caaccc25244572ee9f3
Tom Bendall <t.bendall15@imperial.ac.uk>
2018-06-29 00:52:21 +0100

    fix errors in plotting

--- src/ensemble_plotter.py
+++ src/ensemble_plotter.py
@@ -2,19 +2,45 @@ from netCDF4 import Dataset
 import numpy as np
 from matplotlib import rc
 import matplotlib.pyplot as plt
+import time
+import datetime
 
-data = Dataset('ensemble_spread.nc', 'r')
+def convert_date(date):
+
+    year = int(date[0:4])
+    n = len(date)
+    try:
+        month = int(date[5:7])
+        offset = 7
+    except ValueError:
+        month = int(date[5:6])
+        offset = 6
+
+    day = int(date[offset+1:n])
+
+    return year, month, day
+
+data = Dataset('ensemble_output.nc', 'r')
+
+print('blah')
 
 variables = ['pressure_error', 'temperature_error', 'rainfall_error']
 labels1 = ['Normalised Pressure Error', 'Normalised Temperature Error', 'Normalised Rainfall Error']
 labels2 = ['Mean pressure error', 'Mean temperature error', 'mean rainfall error']
 
 dates_raw = data.variables['date']
-dates = date_convert(dates_raw)
+dates = []
+
+# convert dates
+for i in range(len(dates_raw[:])):
+    year, month, day = convert_date(dates_raw[i])
+    dt = datetime.datetime(year=year, month=month, day=day)
+    dates.append(time.mktime(dt.timetuple()))
+
 
 for variable, label1, label2 in zip(variables, labels1, labels2):
 
-    error = data.variables['variable'][:,:]
+    error = data.variables[variable][:,:]
     full_dates = []
     full_error = []
     mean_error = []
@@ -39,7 +65,7 @@ for variable, label1, label2 in zip(variables, labels1, labels2):
 
     ax.scatter(full_dates, full_error, color='black', marker='o')
     ax.set_xlabel(r'Date', fontsize=28)
-    ax.set_ylabel(label, fontsize=28)
+    ax.set_ylabel(label1, fontsize=28)
 
     fig.savefig('ensemble_scatter_'+str(variable)+'.pdf')
 
@@ -57,7 +83,7 @@ for variable, label1, label2 in zip(variables, labels1, labels2):
 
     ax.scatter(dates, mean_error, color='black', marker='o')
     ax.set_xlabel(r'Date', fontsize=28)
-    ax.set_ylabel(label, fontsize=28)
+    ax.set_ylabel(label2, fontsize=28)
 
     fig.savefig('ensemble_mean_error_'+str(variable)+'.pdf')
     
